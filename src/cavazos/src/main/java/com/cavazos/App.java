e0de860424b9cdfa8bbf6c9fcc05d0224a5e6e73
Vijay Kethanaboyina <112053742+vijaykcsis@users.noreply.github.com>
2022-10-19 15:41:24 -0700

    Added basic apache maven project

---
+++ src/cavazos/src/main/java/com/cavazos/App.java
@@ -0,0 +1,13 @@
+package com.cavazos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
